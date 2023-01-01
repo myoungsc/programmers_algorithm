54c18f5a0254880cb2bac64e34df7f0fd510b234
myoungsc <myoungsc.dev@gmail.com>
2023-01-01 21:19:24 +0900

    부족한 금액 계산하기

--- programers_algorithm/main.swift
+++ programers_algorithm/main.swift
@@ -2,10 +2,24 @@
 //  main.swift
 //  programers_algorithm
 //
-//  Created by 명수창 on 2023/01/01.
+//  Created by myoungsc on 2023/01/01.
 //
 
 import Foundation
 
-print("Hello, World!")
 
+/**
+ 부족한 금액 계산하기
+ https://school.programmers.co.kr/learn/courses/30/lessons/82612
+ Lv. 1
+ */
+/*
+func solution(_ price:Int, _ money:Int, _ count:Int) -> Int64 {
+  var answer:Int64 = Int64(money)
+  for i in 1 ... count {
+    print(price * i)
+    answer -= Int64(price * i)
+  }
+  return answer >= 0 ? 0 : abs(answer)
+}
+*/
